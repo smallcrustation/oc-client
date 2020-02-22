cdd8521a4f7bdf6de2bd66c74016eeeb2addd2f9
krill <lpmaze@gmail.com>
2020-02-22 10:48:30 -0500

    first commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../header/Header';
+
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.scss'
+
+const Header: React.FC = () => {
+  
+  // let nightMode = false
+
+  return (
+  <header className='theme-day' >
+    Head
+  </header>
+  )
+}
+
+export default Header
