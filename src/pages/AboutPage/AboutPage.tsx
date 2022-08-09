c7b04a8435277e834eb36d4fc2f5a1ffe56e677d
smallcrustation <lpmaze@gmail.com>
2022-08-09 18:40:20 -0400

    edited about page text

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -12,7 +12,9 @@ const AboutPage = () => {
       />
       <h1 className="underline-h2">About</h1>
       <p>
-      Oberholtzer Construction was started more than 80 years ago in Ohio and has been building in South Florida since 2005. We develop spec and custom, residential and commercial properties. We offer start to finish development services ranging from finding and closing on property to turnkey construction.
+      Oberholtzer Construction was started more than 80 years ago in Ohio and has been building in South Florida since 2005. 
+      We manage and develop spec/custom, residential and commercial properties. We offer start to finish development services ranging 
+      from finding and closing on property to turnkey construction with care and speed.
       </p>
     </div>
   )
